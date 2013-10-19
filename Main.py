c6a663759fa68b963458d4962fb565ec88c68849
wsycharles0o <wsycharles0o@hotmail.com>
2013-10-18 22:43:58 -0700

    First implementation.
    Signed-off-by: wsycharles0o <wsycharles0o@hotmail.com>

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 
-#from Capture.py import GetFrame
+#from Capture.py import get_frame
 #import GestureRecognition.py
 #import Actions.py
 
@@ -11,32 +11,63 @@
 @type thresholds_list = [int threshold0, [int threshold1, [...]]]
 
 @global gestures_list Gestures: stores all gestures and their related functions
-@global prev_f: the previous frame captured
+@global frame Prev_f: the previous frame captured
 
-@import func GetFrame()
+@import func get_frame()
     returns the current frame.
 
-@func Recognize_Gestrue(frame f)
+@func recognize_gestrue(frame f)
     for each gesture in Gestures call recognize(f) with each frame.
-@func UpdateCounters(thresholds_list l)
+@func update_counters(thresholds_list l)
     for each gesture in Gestures apply l to threshold.
-    Change negative threshold to 0.
-@func CheckCounters()
+    change negative threshold to 0.
+@func check_counters()
     for each gesture in Gestures call action() when threshold > 100 
 @func GUI()
     handles GUI
+@func initialization()
+    build Gestures.
 """
 
 #@type frame
-prev_f = []
+Prev_f = []
 
 #@type gestures_list
 Gestures = []
 
 
-prev_f = GetFrame()
+def recognize_gestrue(frame):
+    global Gestures, Prev_f
+    l = []
+    for g in Gestures:
+        l.append(g[1](Prev_f, frame))
+    prev_f = frame
+    return l
+
+def update_counters(l):
+    global Gestures
+    for i in len(Gestures):
+        Gestures[i][0] += l[i]
+        if Gestures[i][0] < 0:
+            Gestures[i][0] = 0
+
+def check_counters():
+    global Gestures
+    for g in Gestures:
+        if g[0] >= 100:
+            g[2]()
+            g[0] = 0
+
+def GUI():
+    pass
+
+def initialization():
+    pass
+
+initialization()
+Prev_f = get_frame()
 while True:
-    UpdateCounters(Recognize_Gestrue(GetFrame()))
-    CheckCounters()
+    update_counters(recognize_gestrue(get_frame()))
+    check_counters()
     if GUI()
         return 0
